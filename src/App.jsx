803fcea996ef3fe8dd49c3eb70de266cfff406b6
blancojohn <johnmario.bdc@gmail.com>
2024-04-11 18:12:01 -0400

    Configuración inicial OK y Fetch por consola OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Player from './components/Player';
+
 
 const App = () => {
+    fetch('https://playground.4geeks.com/sound/songs')
+        .then((response)=> {
+            console.log(response)
+            return response.json()
+        })
+        .then((datos)=> {
+            console.log(datos)
+            datos
+        })
+        .catch((error)=> console.log(error))
     return (
-        <h1>REACT APP</h1>
+        <>
+            <Player />
+        </>
     )
 }
 
 export default App;
+        
